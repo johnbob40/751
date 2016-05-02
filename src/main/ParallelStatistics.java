22d7114fdc6a0337defcc3f1f98c8a97be13584f
Daniel <DBrocx@gmail.com>
2016-05-02 14:39:01 +1200

    Slight refactoring/renaming and initial set up of method signature

---
+++ src/main/ParallelStatistics.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Collection;
+
+public class ParallelStatistics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+	public static String[] calculate(Collection<?> data, boolean mean, boolean median, boolean max, boolean min, boolean stdDev, boolean intQuartRange, boolean skewness, boolean corrCoef){
+		
+		return null;
+	}
+}
--- test/main/test.java
+++ test/main/test.java
@@ -5,10 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class test {
-
+	
 	@Test
 	public void defaultTest() {
-		assertEquals(1, 1);
+		assertNull(ParallelStatistics.calculate(null, false, false, false, false, false, false, false, false));
 	}
 
+	@Test
+	public void testMean() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void testMaxWithoutMedian() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void testMaxWithMedian() {
+		assertEquals(1, 1);
+	}
+	@Test
+	public void testMinWithoutMedian() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void testMinWithMedian() {
+		assertEquals(1, 1);
+	}
 }
